1742ec77710455c9578d794f2a8e540741d48d01
MAXPC\maxne <maxneumann99@gmail.com>
2025-08-17 12:53:57 +0200

    Added colors from palletteer classic 10 manually

---
+++ R/global_constants.R
@@ -0,0 +1,12 @@
+color_palette <- c(
+  "#1F77B4FF",
+  "#FF7F0EFF",
+  "#2CA02CFF",
+  "#D62728FF",
+  "#9467BDFF",
+  "#8C564BFF",
+  "#E377C2FF",
+  "#7F7F7FFF",
+  "#BCBD22FF",
+  "#17BECFFF"
+)
