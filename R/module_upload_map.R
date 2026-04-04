5810d6cfcacffa7009ae59d7a139ff7b29f560c4
MAXPC\maxne <maxneumann99@gmail.com>
2026-04-04 10:29:23 +0200

    Renamed module_new_loc_overlay to module_upload_map. It centers on the coordinates of an uploaded image, lets the user change map view to alter location and shows all tag coordinates from database on the map with a label showing the thumbnail image on hover

--- R/module_upload_map.R
+++ R/module_upload_map.R
@@ -6,27 +6,7 @@ new_loc_overlay_ui <- function(id) {
       leaflet::leafletOutput(ns("minimap"), height = "100%"),
       shiny::div(
         style = "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 1000; pointer-events: none; color: white; font-size: 30px; text-shadow: 0 0 5px black;",
-        "⌖"
-      )
-    ),
-    shiny::fluidRow(
-      shiny::column(
-        width = 6,
-        shiny::selectizeInput(
-          inputId = ns("tag_name_input"),
-          label = "Tag Name (auswählen oder neu tippen)",
-          choices = NULL,
-          multiple = FALSE,
-          options = base::list(create = TRUE, dropdownParent = "body")
-        )
-      ),
-      shiny::column(
-        width = 6,
-        shiny::dateInput(
-          inputId = ns("date_input"),
-          label = "Aufnahmedatum",
-          value = Sys.Date()
-        )
+        "+"
       )
     )
   )
@@ -36,30 +16,12 @@ new_loc_overlay_server <- function(id, upload_data, pool_con) {
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    shiny::observe({
-      tags_df <- query_data_for_selector(pool_con)
-      shiny::updateSelectizeInput(
-        session = session,
-        inputId = "tag_name_input",
-        choices = tags_df$tag_name,
-        server = TRUE
-      )
-    })
-
     output$minimap <- leaflet::renderLeaflet({
       shiny::req(upload_data())
       data <- upload_data()
 
       lat_init <- base::as.numeric(data$lat[1])
       lng_init <- base::as.numeric(data$lng[1])
-
-      if (!base::is.na(data$date_created[1])) {
-        parsed_date <- base::as.Date(
-          stringr::str_sub(data$date_created[1], 1, 10),
-          format = "%Y:%m:%d"
-        )
-        shiny::updateDateInput(session, "date_input", value = parsed_date)
-      }
 
       all_tags <- query_data_for_selector(pool_con)$tag_id
       existing_locs <- query_locations_for_map(
@@ -75,7 +37,7 @@ new_loc_overlay_server <- function(id, upload_data, pool_con) {
         leaflet::setView(lng = lng_init, lat = lat_init, zoom = 17)
 
       if (base::nrow(existing_locs) > 0) {
-        popup_html <- base::paste0(
+        label_html <- base::paste0(
           "<div style='text-align:center;'>",
           "<strong>",
           existing_locs$tag_name,
@@ -91,11 +53,12 @@ new_loc_overlay_server <- function(id, upload_data, pool_con) {
             data = existing_locs,
             lng = ~lng,
             lat = ~lat,
-            fillColor = ~color,
+            fillColor = "#D3D3D3",
             fillOpacity = 0.4,
             stroke = FALSE,
-            radius = 8,
-            popup = popup_html
+            radius = 5,
+            label = base::lapply(label_html, shiny::HTML),
+            labelOptions = leaflet::labelOptions(direction = "auto")
           )
       }
 
@@ -104,8 +67,6 @@ new_loc_overlay_server <- function(id, upload_data, pool_con) {
 
     return(shiny::reactive({
       base::list(
-        tag_name = input$tag_name_input,
-        date = input$date_input,
         lat = input$minimap_center$lat,
         lng = input$minimap_center$lng
       )
@@ -114,6 +75,8 @@ new_loc_overlay_server <- function(id, upload_data, pool_con) {
 }
 
 new_loc_overlay_app <- function() {
+  shiny::addResourcePath("www", base::file.path(base::getwd(), "inst/app/www"))
+
   ui <- shiny::fluidPage(
     shiny::h3("Test: Bearbeitungs-UI & Mini-Map"),
     new_loc_overlay_ui("overlay1"),
@@ -130,8 +93,7 @@ new_loc_overlay_app <- function() {
     dummy_upload_data <- shiny::reactive({
       base::data.frame(
         lat = 49.4875,
-        lng = 8.4660,
-        date_created = "2023:10:15 14:30:00"
+        lng = 8.4660
       )
     })
 
