2b44e0e36722c743301b651fd57492758bc43568
MAXPC\maxne <maxneumann99@gmail.com>
2026-04-05 16:14:57 +0200

    Implemented upload tab app server logic

--- R/app_server.R
+++ R/app_server.R
@@ -5,7 +5,7 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  pool_con <- open_db_pool("inst/extdata/tom_database.sqlite")
+  pool_con <- open_db_pool("inst/extdata/tom_database2.sqlite")
 
   shiny::onStop(function() {
     pool::poolClose(pool_con)
@@ -37,5 +37,62 @@ app_server <- function(input, output, session) {
 
   shiny::observeEvent(list_click(), {
     shared_selection(list_click())
+  })
+
+  # Upload Tab Logic
+  current_upload_index <- shiny::reactiveVal(1)
+  db_trigger <- shiny::reactiveVal(0)
+  existing_locations <- shiny::reactive({
+    query_locations_for_map(pool_con, selector_data()$tag_id, color_palette)
+  })
+
+  disable_save_btn <- shiny::reactive({
+    base::is.null(upload_inputs$files()) ||
+      current_upload_index() > base::nrow(upload_inputs$files())
+  })
+
+  upload_inputs <- upload_form_server(
+    id = "upload_form",
+    tags_df = selector_data,
+    disable_save = disable_save_btn,
+    current_index = current_upload_index
+  )
+
+  current_upload_image <- shiny::reactive({
+    shiny::req(upload_inputs$files())
+    upload_inputs$files() |> dplyr::slice(current_upload_index())
+  })
+
+  shiny::observeEvent(upload_inputs$skip_click(), {
+    current_upload_index(current_upload_index() + 1)
+  })
+
+  map_center <- upload_map_server(
+    "upload_map",
+    current_upload_image,
+    existing_locations
+  )
+  crop_result <- upload_thumbnail_server(
+    "upload_thumbnail",
+    shiny::reactive(current_upload_image()$temp_datapath)
+  )
+
+  shiny::observeEvent(upload_inputs$files(), {
+    current_upload_index(1)
+  })
+
+  shiny::observeEvent(upload_inputs$save_click(), {
+    shiny::req(current_upload_image(), crop_result(), map_center())
+
+    process_and_save_upload(
+      pool_con = pool_con,
+      current_img = current_upload_image(),
+      crop_data = crop_result()$crop_data,
+      map_center = map_center(),
+      tag_name = upload_inputs$tag(),
+      date_created = as.character(upload_inputs$date())
+    )
+
+    current_upload_index(current_upload_index() + 1)
   })
 }
